82c1947b0eb6e8b560c1aca8cb19c9bc520f6739
Rob Yates <ryates100@gmail.com>
2009-07-03 00:20:14 -0400

    Changed template path to use dynamic os.path instead of hard-wired path

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,5 +1,6 @@
 # Django settings for myproject project.
 
+import os
 #import sys
 #sys.path.append('/home/nxsg/webapps/asawalk/myproject/walk')
 
@@ -73,7 +74,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/nxsg/webapps/asawalk/myproject/walk/templates",
+    os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
