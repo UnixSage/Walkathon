2649c52f65e7d67baf2aef75391fc4c32bca86ad
John C. Place <dev@unixsage.com>
2009-09-02 01:09:03 -0400

    Added username to person display in admin

--- myproject/walk/admin.py
+++ myproject/walk/admin.py
@@ -2,8 +2,8 @@ from walk.models import *
 from django.contrib import admin
 
 class PersonAdmin(admin.ModelAdmin):
-    search_fields = ['first_name', 'last_name', 'email', 'uuid']
-    list_display = ['first_name', 'last_name', 'email', 'uuid']
+    search_fields = ['first_name', 'last_name', 'email', 'username', 'uuid']
+    list_display = ['first_name', 'last_name', 'email', 'username', 'uuid']
 
 class SponsorAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name']
