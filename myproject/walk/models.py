e858d69198ef0f2bbd751637ce7e3a18a90d5b86
John C. Place <dev@unixsage.com>
2009-09-02 01:25:36 -0400

    Change to setting is_captain to False when walker is created.

--- myproject/walk/models.py
+++ myproject/walk/models.py
@@ -32,7 +32,7 @@ class Person(models.Model):
     username = models.CharField(max_length=30, unique=True)
     shirt_size = models.CharField(max_length=15, choices=ShirtSizes)
     team = models.ForeignKey(Team, blank=True, null=True)
-    is_captain = models.BooleanField(default=True)
+    is_captain = models.BooleanField(default=False)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     phone = models.CharField(max_length=50)
